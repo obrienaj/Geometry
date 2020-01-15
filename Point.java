5c83aaf3f5182e6a51ba879c8ce807147ea57687
Allison Jane O Brien <obrienaj@s.dcsdk12.org>
2020-01-15 09:26:31 -0700

    Edit PointTester

--- Point.java
+++ Point.java
@@ -1,3 +1,19 @@
 public class Point{
-    
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    })
+
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+
+    public double distance(Point p, Point q){
+
+        return Math.sqrt((q.getX() - p.getX())*(q.getX() - p.getX()) + (q.getY() - p.getY())*(q.getY() - p.getY()));
+    }
 }
--- PointTester.java
+++ PointTester.java
@@ -1,5 +1,12 @@
 public class PointTester{
     public static void main(String[] args){
 
+        Point p = new Point(1, 1);
+        Point q = new Point(2, 1);
+        Point r = new Point(3, 3);
+
+        System.out.println("Distance between p and q: " + distance(p, q));
+        // given 2 points (x, y), calculate the distance between the points
+        // given 3 points (x, y), calculate area of the triangle
     }
 }
