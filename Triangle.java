e98c20e06027a9df7da82d5538ec916dfaed5916
Cassidy Christian <cassidysparkles@icloud.com>
2020-01-15 09:25:23 -0700

    Add Traingle

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle{
+    private double length; 
+    private double height; 
+
+
+// p=x1,y1  q=x2,y2, r=x3,y3
+    //area=( x1(y2-y3)+x2(y3-y1)+x3(y1-y2))/2
+
+    (p.getX()*(q.getY()-r.getY()))+(q.getX()*(r.getY()-p.getY()))
+
+}
